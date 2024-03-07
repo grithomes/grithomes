8adcddb16d1954b7ff89509c3f1de7816eb4424b
Jdjohar <Jdeep514@gmail.com>
2024-03-07 16:02:56 +0530

    Link update for frontend

--- server/index.js
+++ server/index.js
@@ -12,7 +12,8 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","https://grithomes.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin","https://grit.homes");
+  // res.setHeader("Access-Control-Allow-Origin","https://grithomes.vercel.app");
   // res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
