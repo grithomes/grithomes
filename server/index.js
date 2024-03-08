1298c0ae12fe6c8b03da24868da439f9daf1319d
Jdjohar <Jdeep514@gmail.com>
2024-03-08 23:00:22 +0530

    Update orign links

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   // Allow multiple domains
   const allowedOrigins = [
     "https://grithomes.vercel.app",
-    "https://grit.homes",
+    "https://www.grit.homes",
   ];
 
   const origin = req.headers.origin;
