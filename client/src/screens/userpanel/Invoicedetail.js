6d49a6aac055bd7bbbb814119c5eeb01c7b3c64d
grithomes <162299248+grithomes@users.noreply.github.com>
2024-03-22 18:09:45 +0530

    Update Invoicedetail.js

--- client/src/screens/userpanel/Invoicedetail.js
+++ client/src/screens/userpanel/Invoicedetail.js
@@ -794,6 +794,18 @@ const handleFormSubmit = async (event) => {
         console.log('Email sent successfully!');
         // setShowModal(false);
         setShowEmailAlert(true);
+          // Update the database with emailsent status
+            const updatedData = { ...invoiceData, emailsent: 'yes' }; // Update emailsent status
+            await fetch(`https://grithomes.onrender.com/api/updateinvoicedata/${invoiceid}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(updatedData),
+            });
+
+            // Fetch updated invoice data
+            fetchinvoicedata();
       } else {
         console.error('Failed to send email.');
       }
