f36f97b3c45966dfbf82e90f25c9eb6f75024744
grithomes <162299248+grithomes@users.noreply.github.com>
2024-07-01 12:06:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const mongoDB = require("./db")
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 var path = require('path');
-// const { job } = require('./cron');
+const { job } = require('./cron');
 mongoDB();
 
 // Set maximum payload size limit
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 // Start the cron job
-// job.start();
+job.start();
 
 app.use((req, res, next) => {
   // res.setHeader("Access-Control-Allow-Origin", "https://restro-wbno.vercel.app");
