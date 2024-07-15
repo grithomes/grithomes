ea53dbb740943e0932ff4431190914bdad1407c3
grithomes <162299248+grithomes@users.noreply.github.com>
2024-07-15 15:28:03 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,24 @@
-import {defineConfig} from "vite";
+// import {defineConfig} from "vite";
+// import react from "@vitejs/plugin-react";
+
+
+// export default defineConfig({
+//     plugins: [react()],
+//     server: {
+//         port:5173,
+//         open: true
+// }
+
+
+// });
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port:5173,
-        open: true
-}
-
-
+  plugins: [react()],
+  server: {
+    port: 5173,
+    open: true,
+    historyApiFallback: true, // Add this line
+  },
 });
